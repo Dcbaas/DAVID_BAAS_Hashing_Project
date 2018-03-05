819312f84c817dcbdee680c958a0a812954333eb
Dcbaas <Dcbaas@github.com>
2018-03-05 17:19:30 -0500

    finished loading functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include "Superhero.h"
 
+Superhero superheroBuilder(std::string &input);
 
 int main(int argc, char** argv){
   std::ifstream in{"marvel-wikia-data.csv"};
@@ -22,61 +23,72 @@ int main(int argc, char** argv){
   std::getline(in,input);
  
   while(std::getline(in,input)){
-  
+          Superhero hero = superheroBuilder(input);
 
+        heroDB->push_back(hero);
+    }
+
+    for(auto hero_it(heroDB->begin()); hero_it < heroDB->end(); hero_it++){
+        Superhero output(*hero_it);
+        std::cout << output.toString() << std::endl;
+    }
+
+
+}
+
+
+Superhero superheroBuilder(std::string &input){
     std::string params[13];
 
     std::stringstream data(input);
-    
-    for(int i(0); i < 13; i++)
-      std::getline(data, params[i], ',');
-    
+
+    for(int i(0); i < 13; i++) {
+        std::getline(data, params[i], ',');
+        if(params[i].front() == '"'){
+            while(params[i].back() != '"') {
+                std::string temp;
+                std::getline(data,temp,',');
+                params[i] +=temp;
+            }
+        }}
+
     int page_id = std::stoi(params[0]);
     const std::string name(params[1]);
     const std::string urlslug(params[2]);
     const std::string id(params[3]);
     const std::string alignment(params[4]);
-   
+
     char eye_color('V');
     if(!params[5].empty())
-      eye_color = params[5].at(0);
+        eye_color = params[5].at(0);
 
     char hair_color('V');
     if(!params[6].empty())
-      hair_color = params[6].at(0);
-    
+        hair_color = params[6].at(0);
+
     char sex('U');
     if(!params[7].empty())
-      sex = params[7].at(0);
- 
+        sex = params[7].at(0);
+
     const std::string gsm(params[8]);
 
     int appearances(1);
     if(!params[10].empty())
-      appearances = std::stoi(params[10]);
-    
+        appearances = std::stoi(params[10]);
+
     const std::string first_apperance(params[11]);
-   
+
     int year(0);
-    if(!params[12].empty())   
-       year = std::stoi(params[12]);
+    if(!params[12].empty())
+        year = std::stoi(params[12]);
 
     bool alive(true);
-    if(params[9].at(0) == 'D')
-      alive = false;
+    if( !params[9].empty() && params[9].at(0) == 'D')
+        alive = false;
 
+    Superhero hero(page_id, name, urlslug, id, alignment, eye_color,
+                   hair_color, sex, gsm, alive, appearances, first_apperance, year);
 
-  Superhero hero(page_id, name, urlslug, id, alignment, eye_color,
-  hair_color, sex, gsm, alive, appearances, first_apperance, year);
-	 
-  heroDB->push_back(hero);   
-  }
-
-  for(auto hero_it(heroDB->begin()); hero_it < heroDB->end(); hero_it++){
-   Superhero output(*hero_it);
-   std::cout << output.toString() << std::endl;
-  }
-
-  
+    return hero;
 }
 

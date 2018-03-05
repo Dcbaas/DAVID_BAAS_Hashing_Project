466910749aa12941c8b2691c91439c65fed24ecb
Dcbaas <Dcbaas@github.com>
2018-03-04 22:48:15 -0500

    Added some Setters and Getters

--- include/Superhero.h
+++ include/Superhero.h
@@ -11,55 +11,55 @@ public:
               const std::string &alignment, char eye_color, char hair_color, char sex, const std::string &gsm,
               bool alive, int appearances, const std::string &first_apperance, int year);  
 
-  void setPageID(int page_id);
+  void setPageID(int &page_id);
 
   int getPageID() const;
 
-  void setName(std::string name);
+  void setName(std::string &name);
 
   std::string getName() const;
 
-  void setUrlSlug(std::string url_slug);
+  void setUrlSlug(std::string &urlslug);
 
   std::string getUrlslug() const;
 
-  void setID(std::string id);
+  void setID(std::string &id);
 
   std::string getID() const;
 
-  void setAlignment(std::string alignment);
+  void setAlignment(std::string &alignment);
 
   std::string getAlignment() const;
 
-  void setEyeColor(char eye_color);
+  void setEyeColor(char &eye_color);
 
   char getEyeColor() const;
 
-  void setHairColor(char hair_color);
+  void setHairColor(char &hair_color);
 
   char getHairColor() const;
 
-  void setSex(char sex);
+  void setSex(char &sex);
 
   char getSex() const; //should this be changed to be less dirty?
 
-  void setGSM(std::string gsm);
+  void setGSM(std::string &gsm);
 
   std::string getGSM() const;
 
-  void setAlive(bool alive);
+  void setAlive(bool &alive);
 
   bool getAlive() const;
 
-  void setApperances(int appearances);
+  void setApperances(int &appearances);
 
   int getApperances() const;
 
-  void setFirstApperance(std::string first_apperance);
+  void setFirstApperance(std::string &first_apperance);
 
   std::string getFristApperance() const;
 
-  void setYear(int year);
+  void setYear(int &year);
 
   int getYear() const;
 
--- src/Superhero.cpp
+++ src/Superhero.cpp
@@ -8,3 +8,46 @@ Superhero::Superhero(int page_id, const std::string &name, const std::string &ur
         page_id(page_id), name(name),urlslug(urlslug), id(id), alignment(alignment), eye_color(eye_color),
         hair_color(hair_color), sex(sex), gsm(gsm), alive(alive), appearances(appearances), first_apperance(first_apperance),
         year(year) {}
+
+
+void Superhero::setPageID(int &page_id){
+  Superhero::page_id = page_id;
+}
+
+int Superhero::getPageID() const{
+  return page_id;
+}
+
+void Superhero::setName(std::string &name){
+  Superhero::name = name;
+}
+
+std::string Superhero::getName() const{
+  return name;
+}
+
+void Superhero::setUrlSlug(std::string &urlslug){
+  Superhero::urlslug = urlslug; 
+}
+
+std::string Superhero::getUrlSlug() const{
+  return urlslug;
+}
+
+void Superhero::setID(std::string &id){
+  Superhero::id = id;
+}
+
+std::string Superhero::getID() const{
+  return id;
+}
+
+void Superhero::setAlighment(std::string &alignment){
+  Superhero::alignment = alignment;
+}
+
+std::string Superhero::getAlignment() const{
+  return alignment;
+}
+
+
